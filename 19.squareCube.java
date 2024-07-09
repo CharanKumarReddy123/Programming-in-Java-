501a81cd2200c11406c0a020b63e8d0e631a56c4
CharanKumarReddy123 <130465307+CharanKumarReddy123@users.noreply.github.com>
2024-07-09 09:19:37 +0530

    Create 19.squareCube.java

---
+++ 19.squareCube.java
@@ -0,0 +1,13 @@
+ import java.util.Scanner;
+ class squareCube{
+    public static void main(String args[]){
+      Scanner input=new Scanner(System.in);
+      float n=input.nextFloat();
+      System.out.println("square: "+(n*n));
+      System.out.print("cube: "+(n*n*n));
+    }
+}
+output:
+6.0
+square: 36.0
+cube: 216.0
